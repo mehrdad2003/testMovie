f3a988c44bcf7e83ef77aab9d7b399372e905ee2
unknown <mehrdadzare2003@gmail.com>
2021-12-18 07:59:40 +0330

    fir

---
+++ public/allScripts/class.js
@@ -0,0 +1,47 @@
+class Physics {
+    constructor(speedX = 0, speedY = 0, gravity = .2, myGravity = 0, airResistance = .03, power = 0) {
+        this.speedX = speedX;
+        this.speedY = speedY;
+        this.gravity = gravity;
+        this.myGravity = myGravity;
+        this.airResistance = airResistance;
+        this.throwPower = [];
+        this.power = power;
+
+    }
+    crash = (obj) => crashFlag(this, obj);
+    bounce = () => bounce(this);
+    airSlow = () => airSlow(this);
+    throwEl = () => throwEl(this);
+    countPower = () => countPower(this);
+}
+
+class RectClass extends Physics {
+    constructor(x = 130, y = 0, color = "#22a", width = 20, height = 20, rotateZ = 0, speedX, speedY, gravity) {
+        super(speedX, speedY, gravity);
+
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.width = width;
+        this.height = height;
+        this.rotateZ = rotateZ;
+
+        this.active = 0;
+        this.disableThrow = 1;
+    }
+    draw = () => draw(this);
+    newPos = () => newPos(this);
+    clicked = (a, b) => clicked(this, a, b);
+}
+
+
+class AllObjects extends RectClass {
+    constructor(x, y, color, width, height, kindOfObcject, show = 1, showBonus = 1) {
+        super(x, y, color, width, height);
+        this.kindOfObject = kindOfObcject;
+        this.show = show;
+        this.showBonus = showBonus;
+    }
+    song = (audio) => audio.play();
+}
---
+++ public/allScripts/elementPosition.js
@@ -0,0 +1,20 @@
+const elementPosition = "" +
+    "-HK----K-rrr" +
+    "Qpp---Hp-wgw" +
+    "pppw-wpr-wKw" +
+    "ppwg-gwr-wgw" +
+    "pwgw-wgw-wEw" +
+    "HgwQ-Qwg-ggg" +
+    "gwKw-wKw-wHw" +
+    "wQwg-gwQ-wgw" +
+    "rEErKwgpHwQw" +
+    "rggrgrwggrgr";
+
+
+// E - eggs
+// H - pig helmet
+// Q - pig
+// K - pig king
+// w - wood
+// g - glass
+// r - rock
